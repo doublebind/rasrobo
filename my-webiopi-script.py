27d71b3d2198ccbb5be967c2fa70a1f6990b5149
doublebind <zero2one.codeclub@gmail.com>
2016-05-14 19:13:44 +0900

    Made the default for python3.  Lines for python2 are commented out.

--- my-webiopi-script.py
+++ my-webiopi-script.py
@@ -1,8 +1,10 @@
 import webiopi
 import os
-import urllib
+import urllib.parse  # python3
+#import urllib       # python2
 
 @webiopi.macro
 def jtalk(text):
-        text_decode = urllib.unquote(text)
+        #text_decode = urllib.unquote(text)    #python2
+        text_decode = urllib.parse.unquote(text)    #python3
         os.system("/home/pi/jtalk.sh " + text_decode)
